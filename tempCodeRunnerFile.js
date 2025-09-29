8ebed8384e96d9d567b7260c67d2ee9e59fa6dd2
ChanVimean <mrrmeanvlogger@gmail.com>
2025-09-29 19:59:06 +0700

    add Day 12

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,6 @@
+const students = { name: "John", age: 20, isActive: true }
+
+console.log(students.name + " " + students.age + " " + students.isActive);
+console.log(`${students.name} ${students.age} ${students.isActive}`);
+console.log(students.age);
+console.log(students.isActive);
