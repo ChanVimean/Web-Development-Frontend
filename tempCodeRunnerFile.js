d385187330efbc44c4a2297702c0e9801f301be9
ChanVimean <mrrmeanvlogger@gmail.com>
2025-10-01 18:51:24 +0700

    Add sync

--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,6 +1,21 @@
-const students = { name: "John", age: 20, isActive: true }
+const chore1 = () => {
+  setTimeout(() => {
+    console.log("Task 1 completed");
+  }, 1000);
+};
 
-console.log(students.name + " " + students.age + " " + students.isActive);
-console.log(`${students.name} ${students.age} ${students.isActive}`);
-console.log(students.age);
-console.log(students.isActive);
+const chore2 = () => {
+  setTimeout(() => {
+    console.log("Task 2 completed");
+  }, 2000);
+};
+
+const chore3 = () => {
+  setTimeout(() => {
+    console.log("Task 3 completed");
+  }, 500);
+};
+
+chore1(); // Starts and waits 1s (in background)
+chore2(); // Starts and waits 2s (in background)
+chore3(); // Starts and waits 0.5s (in background)
